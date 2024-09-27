09d5d3ffb5960f3dd6966835fe26b8be2e41a1a3
Revalinazwa <119474745+Revalinazwa@users.noreply.github.com>
2024-09-27 09:05:14 +0700

    Pertemuan 3 - Modul 2 dan 3, tapi ada bug

--- app/src/main/java/lat/pam/hellotoast/MainActivity.kt
+++ app/src/main/java/lat/pam/hellotoast/MainActivity.kt
@@ -8,17 +8,15 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private var mCount = 0;
+    private var mCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val mShowCount = findViewById<TextView>(R.id.show_count)
         val buttonCountUp = findViewById<Button>(R.id.button_count)
@@ -26,30 +24,27 @@ class MainActivity : AppCompatActivity() {
         val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
         val buttonBrowser = findViewById<Button>(R.id.button_browser)
 
-        buttonCountUp.setOnClickListener(View.OnClickListener {
-            mCount++;
-            Log.d("mCount", Integer.toString(mCount))
-            if (mShowCount != null)
-                mShowCount.text = mCount.toString()
-        })
+        buttonCountUp.setOnClickListener {
+            mCount++
+            Log.d("MainActivity", mCount.toString())
+            mShowCount.text = mCount.toString()
+        }
 
-        buttonToast.setOnClickListener(View.OnClickListener {
-            val tulisan: String = mShowCount?.text.toString()
-            val toast: Toast = Toast.makeText(this, "Angka yang dimunculkan "
-                    + tulisan, Toast.LENGTH_LONG)
-                    toast.show()
-        })
+        buttonToast.setOnClickListener {
+            val tulisan: String = mShowCount.text.toString()
+            Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG).show()
+        }
 
-        buttonSwitchPage.setOnClickListener(View.OnClickListener {
+        buttonSwitchPage.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
-        })
+        }
 
-        buttonBrowser.setOnClickListener(View.OnClickListener {
+        buttonBrowser.setOnClickListener {
             val intentbrowse = Intent(Intent.ACTION_VIEW)
-            intentbrowse.setData(Uri.parse("https://www.google.com/"))
+            intentbrowse.data = Uri.parse("https://www.google.com/")
             startActivity(intentbrowse)
-        })
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
